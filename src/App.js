67919985fbafa96dea357831c49aa30d6197b493
osefatul <sefaullahomar2@gmail.com>
2021-09-26 23:30:09 -0700

    everything is done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Single from "./Pages/Single/Single";
 import Write from "./Pages/Write/Write";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
+  const user = false;
   return (
     <div className="App">
       <Router>
@@ -15,18 +16,10 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/write">
-            <Write />
-          </Route>
-          <Route path="/settings">
-            <Settings />
-          </Route>
+          <Route path="/register">{user ? <Home /> : <Register />}</Route>
+          <Route path="/login">{user ? <Home /> : <Login />}</Route>
+          <Route path="/write">{user ? <Home /> : <Write />}</Route>
+          <Route path="/settings">{user ? <Home /> : <Settings />}</Route>
           <Route path="/post/:postId">
             <Single />
           </Route>
--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -1,32 +1,68 @@
-import React from 'react'
-import './topbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./topbar.css";
 function Topbar() {
+  const user = false;
   return (
     <div className="top">
       <div className="topLeft">
-      <i className="topIcon fab fa-facebook-square"></i>
-      <i className="topIcon fab fa-twitter"></i>
-      <i className="topIcon fab fa-instagram"></i>
-      <i className="topIcon fab fa-pinterest"></i>
-
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter"></i>
+        <i className="topIcon fab fa-instagram"></i>
+        <i className="topIcon fab fa-pinterest"></i>
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
-
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              WRITER
+            </Link>
+          </li>
+          <li className="topListItem">{user && "LOGOUT"}</li>
         </ul>
       </div>
       <div className="topRight">
-        <img className="topImage" src="https://www.wagrati.eu/media/images/flagge-afghanistan-1400x933.jpg" alt="" />
+        {user ? (
+          <img
+            className="topImage"
+            src="https://www.wagrati.eu/media/images/flagge-afghanistan-1400x933.jpg"
+            alt=""
+          />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+
         <i className="topSearchIcon fas fa-search"></i>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Topbar
+export default Topbar;
