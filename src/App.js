5629ef3495604d21e78c67f737f909f27110c887
osefatul <sefaullahomar2@gmail.com>
2021-09-26 22:48:00 -0700

    the router stage is deployed

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import Register from "./Pages/register/Register";
 import Settings from "./Pages/settings/Settings";
 import Single from "./Pages/Single/Single";
 import Write from "./Pages/Write/Write";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Topbar />
-      {/* <Single /> */}
-      {/* <Write /> */}
-      {/* <Settings /> */}
-      <Register />
+      <Router>
+        <Topbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/write">
+            <Write />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route path="/post/:postId">
+            <Single />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
