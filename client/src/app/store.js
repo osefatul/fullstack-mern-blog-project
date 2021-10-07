e8ff5b93efc875e41ec535305f3f5179bd842563
osefatul <sefaullahomar2@gmail.com>
2021-10-06 21:52:01 -0700

    login, app page updated using localstorage to persist login data

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/userSlice";
+import userReducer from "../features/";
 
 export const store = configureStore({
   reducer: {
